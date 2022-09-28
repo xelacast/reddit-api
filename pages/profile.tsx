2e9bbbe605c5da01718e86cb3f05b4c5725a48cf
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-27 18:25:35 -0600

    created profile main page

---
+++ pages/profile.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Search from '../components/search';
+import DataDisplay from '../components/dataDisplay';
+
+import styles from '../styles/profile.module.scss';
+
+const Profile = () => {
+  const [user, setUser] = useState('');
+
+  return (
+    <section id="profile">
+      <div className={styles.search_bar}>
+        <Search setUser={setUser} />
+        <h2>{user}</h2>
+      </div>
+      <div>
+        <DataDisplay user={user} />
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
