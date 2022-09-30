4f756f9cc6f6815db4688b08c5acf610cd94b623
Alexander Castillo <jhuiicebox@gmail.com>
2022-09-30 15:11:17 -0600

    reformated layout grid added header

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,10 +1,14 @@
 import NavBar from '../nav/navbar';
 import Footer from '../footer';
 import styles from './layout.module.scss';
+import Header from '../header';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
+      <header className={styles.header}>
+        <Header />
+      </header>
       <nav className={styles.nav}>
         <NavBar />
       </nav>
