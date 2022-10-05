f126162936408b0b85ca3e1a8fcae490f743f4c4
Alexander Castillo <jhuiicebox@gmail.com>
2022-10-04 19:37:42 -0600

    fixed pixel ratio for search

--- pages/subreddits.tsx
+++ pages/subreddits.tsx
@@ -9,48 +9,7 @@ import Page from '../assets/newspaper-outline.svg';
 import Face from '../assets/person-circle-outline.svg';
 import Time from '../assets/timer-outline.svg';
 import DataBox from '../components/utils/dataBox';
-
-const headerData = {
-  title: 'title',
-  subs: '245342',
-  active: '123',
-  language: 'en-us',
-  created: 'Jan 5th, 1999',
-  filler: 'Filler Data',
-  text: 'Ipsuom asdf as fasd fnlja asdlk jk ds jka j adaskjflasdf kmasd flak al ksd',
-};
-
-const postData = [
-  {
-    title: 'oh yeah baby mamam Garcia',
-    author: 'jhuiice',
-    created: '1234254',
-    score: '123',
-    body: 'asdfasd',
-    ups: '69',
-    downs: '420',
-    num_comments: '453',
-  },
-  {
-    title: 'oh yeah baby mamam Garcia',
-    author: 'jhuiice',
-    created: '1234254',
-    score: '123',
-    body: 'asdfasd',
-    ups: '69',
-    downs: '420',
-    num_comments: '453',
-  },
-];
-
-// const Icons = {
-//   Heart: [Heart, ],
-//   Friend: Friend,
-//   Chart: Chart,
-//   Page: Page,
-//   Face: Face,
-//   Time: Time,
-// };
+import { headerData, postData } from '../utils/fillerData';
 
 const Subreddits = () => {
   const [subreddit, setSubreddit] = useState('');
